2736e07aa686a7460a9d8cba104a86ea72e5fb69
shubhi <shubhi@awakeinfotech.com>
2024-12-04 18:58:26 +0530

    just made the customsafeara globally and brought props to restauarnt screen.

---
+++ src/components/global/CustomSafeAreaView.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomSafeAreaView = () => {
+  return (
+    <View>
+      <Text>CustomSafeAreaView</Text>
+    </View>
+  )
+}
+
+export default CustomSafeAreaView
--- src/features/restaurants/RestaurantScreen.tsx
+++ src/features/restaurants/RestaurantScreen.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React, { FC } from 'react'
+import {View, Text, Platform} from 'react-native';
+import React, {FC} from 'react';
+import {useRoute} from '@react-navigation/native';
+import {useStyles} from 'react-native-unistyles';
+import {restaurantHeaderStyles} from '@unistyles/restuarantStyles';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import CustomSafeAreaView from '@components/global/CustomSafeAreaView';
 
-const RestaurantScreen :FC = () => {
+const RestaurantScreen: FC = () => {
+  const route = useRoute() as any;
+  const restaurant = route?.params?.item;
+  const {styles} = useStyles(restaurantHeaderStyles);
+  const insets = useSafeAreaInsets();
   return (
-    <View>
-      <Text>RestaurantScreen</Text>
-    </View>
-  )
-}
+    <>
+      <View style={{height: Platform.OS === 'android' ? insets.top : 0}}>
 
-export default RestaurantScreen
+        <CustomSafeAreaView> 
+
+        </CustomSafeAreaView>
+      </View>
+    </>
+  );
+};
+
+export default RestaurantScreen;
